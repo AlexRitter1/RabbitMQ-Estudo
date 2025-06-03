292f4ccf8039551feee8226048db427f2bed1ad3
Alex Ritter <ritter.alex@hotmail.com>
2025-06-02 21:49:27 -0300

    Commit inicial

---
+++ mail_microservice/src/test/java/com/ritter/mail_microservice/MailMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritter.mail_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
